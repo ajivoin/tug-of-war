62aca271c49bf0552d89743b83ce2931f763a4d0
Andrew Jivoin <andrew@jivoin.com>
2021-03-13 00:16:30 -0500

    Frick again!

--- util/utils.js
+++ util/utils.js
@@ -11,8 +11,7 @@ const hasProperty = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, pro
  * @param {Number} min
  */
 const getRandomInt = (min, max) => {
-  if (max < min) [min, max] = [max, min];
-  return Math.random() * (max - min) + min; ;
+  return Math.random() * (max - min) + min;
 };
 
 /**
