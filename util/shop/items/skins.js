bbf8f231d23fc7cfb7c6fb4bc6edf7c012bb864d
Andrew Jivoin <14353390+ajivoin@users.noreply.github.com>
2021-03-14 16:33:10 -0400

    Update skins.js
    Removes `skin-` prefix on each item.

--- util/shop/items/skins.js
+++ util/shop/items/skins.js
@@ -23,121 +23,121 @@ export const skins = {
     enabled: false,
     emoji: '🍻',
   },
-  'skin-default': {
+  'default': {
     price: 0,
     description: '✅ Simple.',
     enabled: true,
     emoji: '✅',
   },
-  'skin-devil': {
+  'devil': {
     price: 666,
     description: "😈 Let's make a deal.",
     enabled: true,
     emoji: '😈',
   },
-  'skin-mad': {
+  'mad': {
     price: 500,
     description: '😡 My face when I type the wrong number.',
     enabled: true,
     emoji: '😡',
   },
-  'skin-rage': {
+  'rage': {
     price: 1000,
     description: '🤬 @!#?@!.',
     enabled: true,
     emoji: '🤬',
   },
-  'skin-flex': {
+  'flex': {
     price: 1000,
     description: '💪 Do you even lift?',
     enabled: true,
     emoji: '💪',
   },
-  'skin-pancake': {
+  'pancake': {
     price: 1000,
     description: '🥞 Breakfast is served.',
     enabled: false,
     emoji: '🥞',
   },
-  'skin-megaflex': {
+  'megaflex': {
     price: 2500,
     description: '❓ We get it. You lift.',
     enabled: true,
     emoji: '🌌',
   },
-  'skin-ultraflex': {
+  'ultraflex': {
     price: 5000,
     description: '❓ Gives ability to throw competition into space.',
     enabled: true,
     emoji: '🚀',
   },
-  'skin-hyperflex': {
+  'hyperflex': {
     price: 10000,
     description: "❓ It's all in the name.",
     enabled: true,
     emoji: '🍆',
   },
-  'skin-monke': {
+  'monke': {
     price: 500,
     description: '🐵 MONKE.',
     enabled: false,
     emoji:'🐵',
   },
-  'skin-brain': {
+  'brain': {
     price: 700,
     description: '🧠 Big brain plays only.',
     enabled: false,
     emoji:'🧠',
   },
-  'skin-sparkle': {
+  'sparkle': {
     price: 800,
     description: '✨ Shining bright!',
     enabled: false,
     emoji:'✨',
   },
-  'skin-bug': {
+  'bug': {
     price: 1e23,
     description: '🐛',
     enabled: false,
     emoji:'💩',
   },
-  'skin-wiz': {
+  'wiz': {
     price: 0,
     description: '',
     enabled: false,
     emoji:'🧙‍♂️',
   },
-  'skin-debug': {
+  'debug': {
     price: 0,
     description: "🤖 You shouldn't have this.",
     enabled: false,
     emoji:'🤖',
   },
-  'skin-trees': {
+  'trees': {
     price: 420,
     description: '🍁 Blaze it.',
     enabled: false,
     emoji:'🍁',
   },
-  'skin-clown': {
+  'clown': {
     price: 999,
     description: '🤡 Quit clowning around.',
     enabled: false,
     emoji:'🤡',
   },
-  'skin-koala': {
+  'koala': {
     price: 750,
     description: '🐨 Very cute yet very unintelligent.',
     enabled: false,
     emoji:'🐨',
   },
-  'skin-kiss': {
+  'kiss': {
     price: 696,
     description: '💋 Smooch.',
     enabled: false,
     emoji:'💋',
   },
-  'skin-ufo': {
+  'ufo': {
     price: 1337,
     description: '🛸 Somewhere out there...',
     enabled: false,
